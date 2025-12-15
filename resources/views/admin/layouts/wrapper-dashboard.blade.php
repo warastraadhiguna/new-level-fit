0874e2e44d0ded0a9fb3033310c2b49a431993e4
anglingkusumandhita <anglingkusumandhita@gmail.com>
2025-12-15 09:36:39 +0700

    fix error sidebarr name

--- resources/views/admin/layouts/wrapper-dashboard.blade.php
+++ resources/views/admin/layouts/wrapper-dashboard.blade.php
@@ -2,7 +2,7 @@
 @include('admin.layouts.content');
 {{-- @include('admin.layouts.sidebar-chatbox'); --}}
 @include('admin.includes.header-dashboard');
-@include('admin.includes.sidebarr');
+@include('admin.includes.sidebar');
 {{-- @include('admin.includes.left-sidebar'); --}}
 @include('admin.includes.wallet-sidebar');
 @include('admin.includes.footer-dashboard');
